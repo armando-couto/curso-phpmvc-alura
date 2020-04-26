19f63cb3af92378102ab8e76ef4d477558bf42c4
Armando Couto <coutoarmando@gmail.com>
2020-04-26 03:04:39 -0300

    Corrige o commit da aula 3.

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -7,30 +7,30 @@ use Alura\Cursos\Infra\EntityManagerCreator;
 
 class Persistencia implements InterfaceControladorRequisicao
 {
-    /**
-     * @var \Doctrine\ORM\EntityManagerInterface
-     */
-    private $entityManager;
+  /**
+   * @var \Doctrine\ORM\EntityManagerInterface
+   */
+  private $entityManager;
 
-    public function __construct()
-    {
-        $this->entityManager = (new EntityManagerCreator())
-            ->getEntityManager();
-    }
+  public function __construct()
+  {
+    $this->entityManager = (new EntityManagerCreator())
+      ->getEntityManager();
+  }
 
-    public function processaRequisicao(): void
-    {
-        $descricao = filter_input(
-            INPUT_POST,
-            'descricao',
-            FILTER_SANITIZE_STRING
-        );
+  public function processaRequisicao(): void
+  {
+    $descricao = filter_input(
+      INPUT_POST,
+      'descricao',
+      FILTER_SANITIZE_STRING
+    );
 
-        $curso = new Curso();
-        $curso->setDescricao($descricao);
-        $this->entityManager->persist($curso);
-        $this->entityManager->flush();
+    $curso = new Curso();
+    $curso->setDescricao($descricao);
+    $this->entityManager->persist($curso);
+    $this->entityManager->flush();
 
-        header('Location: /listar-cursos', true, 302);
-    }
+    header('Location: /listar-cursos', true, 302);
+  }
 }
