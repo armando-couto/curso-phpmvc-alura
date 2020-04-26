f3c3db26921c477b6b589bbda3c8c9b8b9819716
Armando Couto <coutoarmando@gmail.com>
2020-04-26 03:00:14 -0300

    Aula 3

--- public/index.php
+++ public/index.php
@@ -4,16 +4,19 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\Persistencia;
 
 switch ($_SERVER['PATH_INFO']) {
-    case '':
-    case '/':
     case '/listar-cursos':
         $controlador = new ListarCursos();
         $controlador->processaRequisicao();
         break;
     case '/novo-curso':
         $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
+        break;
+    case '/salvar-curso':
+        $controlador = new Persistencia();
         $controlador->processaRequisicao();
         break;
     default:
